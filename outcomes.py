ea7827608a854838fd26ef70f81a60fe41e9bae9
timeonator <timeonator@gmail.com>
2021-07-05 02:47:09 -0700

    restored a better version

--- outcomes.py
+++ outcomes.py
@@ -2,14 +2,15 @@ import e3db
 from e3db.types import Search
 from tzutils import read_client_id
 
-client = e3db.Client(e3db.Config.load('alicia'))
-client.share('moves',read_client_id('clarence'))
-client = e3db.Client(e3db.Config.load('bruce'))
-client.share('moves',read_client_id('clarence'))
+# sharing causes an error once a record has been written
+    # raise errors[response.status_code]e3db.exceptions.
+    # ConflictError: Error during API operation: 
+    # Conflict error: Existing item cannot be modified: HTTP 409
+# But the initial share worked without error and functionally.
+# So leave this commented out until issue is resolved. Alternately
+# setup sharing as part of the game setup.
+client = e3db.Client(e3db.Config.load('clarence'))
+# client.share('outcome',read_client_id('alicia'))
+# client.share('outcome',read_client_id('bruce'))
 
-
-client = e3db.Client(e3db.Config.load('bruce'))
-client.write('moves', data={'bruce','scissors'}, plain={'round':'1'})
-
-client = e3db.Client(e3db.Config.load('alicia'))
-client.write('moves', data={'alicia','scissors'}, plain={'round':'1'})
+client.write('outcomes', data={'bruce':'scissors'}, plain={'round':'0'})
